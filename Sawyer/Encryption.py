870763cb138901f3623d77ab5c5eb42ea7372a63
PythonCoderforlife <56777409+PythonCoderforlife@users.noreply.github.com>
2019-10-20 12:18:43 -0500

    Updated Encryption

--- Sawyer/Encryption.py
+++ Sawyer/Encryption.py
@@ -1,4 +1,4 @@
-print("put 2 to 3 characters in then press enter and then,repeat 3 more times.HAS TO INCLUDE A I ")
+print("put 2 to 3 characters in then press enter and then,repeat 3 more times. ")
 
 print("")
 
@@ -6,18 +6,33 @@ base1 = input("")
 base2 = input("")
 base3 = input("")
 base4 = input("")
-cbase1 = len(base1)
-cbase2 = len(base2)
-cbase3 = len(base3)
-cbase4 = len(base4)
+
+
+import math
+
+print(len(base1))
+cbase1 = base1
+
+Upbase1 = (base1.upper())
+Upbase2 = (base2.upper())
+Lobase3 = (base3.lower())
+Lobase4 = (base4.lower())
 
 print("")
 
-Ebase1 = print(base1.replace(I,cbase1))
-Ebase2 = print()
-Ebase3 = print()
-Ebase4 = print()
+Random_numbers1 =  13579
+Random_numbers2 =  24680
+Random_numbers3 =  14709
+Random_numbers4 =  12457
+Ebase1 = ( Random_numbers1 + len(base1))
+Ebase2 = ( Random_numbers2 + len(base2))
+Ebase3 = ( Random_numbers3 + len(base3))
+Ebase4 = ( Random_numbers4 + len(base4))
 
 print("")
-print(Ebase1)
+
+print(Ebase1 , Upbase1 , base1)
+print(Ebase2 , Upbase2 , base2 )
+print(Ebase3 , Lobase3 , base3 )
+print(Ebase4 , Lobase4 , base4 )
 
