8bb222844ff0391feeb69c869edf700d25b3705e
Cwp1701 <54554650+Cwp1701@users.noreply.github.com>
2021-02-22 23:51:07 -0600

    Out with the forge, in with the Fabric

---
+++ src/main/java/com/cporter/first_mod_again/FirstModAgain.java
@@ -0,0 +1,10 @@
+package com.cporter.first_mod_again;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FirstModAgain implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
